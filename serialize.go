7eaf9e883ad0e297c745aa16df2e902792c78241
Eric Fritz <eric@eric-fritz.com>
2020-09-13 15:25:27 -0500

    WIP

--- serialize.go
+++ serialize.go
@@ -12,13 +12,13 @@ func serializeEvent(event interface{}) ([]byte, error) {
 	buffer := bytes.Buffer{}
 
 	c++
-	if _, err := buffer.Write([]byte(fmt.Sprintf("id:%d\n", c))); err != nil {
+	if _, err := buffer.Write([]byte(fmt.Sprintf("id: %d\n", c))); err != nil {
 		return nil, err
 	}
-	if _, err := buffer.Write([]byte("event:temp\n")); err != nil {
+	if _, err := buffer.Write([]byte("event: temp\n")); err != nil {
 		return nil, err
 	}
-	if _, err := buffer.Write([]byte("data:")); err != nil {
+	if _, err := buffer.Write([]byte("data: ")); err != nil {
 		return nil, err
 	}
 
